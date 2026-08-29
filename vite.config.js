4850500f79f6e853f1bcfceb5c95d2a858f07f74
Sayan <forcollagework5@gmail.com>
2026-08-29 18:10:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://devonyx1.vercel.app/',
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   // port: 5173,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://devonyx1.vercel.app/',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 })
